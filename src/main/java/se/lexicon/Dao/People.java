9d1ff3538e43f938d0a91e3024fde54b5f1e5979
aman adinlid <95pinkpanda@gmail.com>
2021-02-17 20:12:30 +0100

    Creating methods for interfaces

--- src/main/java/se/lexicon/Dao/People.java
+++ src/main/java/se/lexicon/Dao/People.java
@@ -1,4 +1,22 @@
 package se.lexicon.Dao;
 
+import se.lexicon.model.Person;
+
+import java.util.Collection;
+
 public interface People {
+
+    Person create(Person person);
+
+    Collection<Person> findAll();
+
+    Person findById(int id);
+
+    Collection<Person> findByName(String Name);
+
+    Person update(Person person);
+
+    boolean deleteById(int id);
+
+
 }
